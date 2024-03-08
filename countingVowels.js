5fd9880d5feade391abe4eb7d26317f936063021
M.Bonheur <mbonheurc23@gmail.com>
2024-03-08 06:29:29 +0200

    initial commit

---
+++ countingVowels.js
@@ -0,0 +1,16 @@
+function countVowels(sentence){
+  let count = 0 ;
+  let vowels = ['a','e','i','u','o'];
+  let splitted = sentence.split('');
+ for(let i = 0 ;i < splitted.length ; i++){
+  for(let j = 0 ; j < vowels.length ; j++){
+    if(splitted[i] === vowels[j]) count++
+  }
+ }
+
+  return count;
+}
+
+  const country = 'Netherland';
+
+  console.log(countVowels(country));
