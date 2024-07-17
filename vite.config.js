14a79d8467303d8e2eb5ee68882a306d448bd94f
M.Bonheur <mbonheurc23@gmail.com>
2024-07-17 11:20:21 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public', // ensure this matches the directory you want to use
-  },
   server: {
     mimeTypes: {
       // ensure .js files are served with the correct MIME type
